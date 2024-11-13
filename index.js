1440b93adf2bb18f814f0713a262d9e9c6986f00
Rishi <54655745+RISHI-1704@users.noreply.github.com>
2024-11-13 11:48:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,17 +8,14 @@ const dateInput = document.getElementById('floatingDate');
 const designationInput = document.getElementById('floatingInputDesignation');
 const ageInput = document.getElementById('floatingInputAge');
 
-// Initially show the form and hide the "Add New" button
 form.style.display = 'block';
 addButton.style.display = 'none';
 
-// Function to toggle visibility of the form and "Add New" button
 function toggleFormVisibility() {
     form.style.display = form.style.display === 'none' ? 'block' : 'none';
     addButton.style.display = addButton.style.display === 'none' ? 'block' : 'none';
 }
 
-// Save button functionality
 saveButton.addEventListener('click', function () {
     if (validateForm()) {
         const table = document.querySelector('tbody');
@@ -34,24 +31,20 @@ saveButton.addEventListener('click', function () {
 
         table.appendChild(newRow);
 
-        // Clear the form fields
+      
         nameInput.value = '';
         dateInput.value = '';
         designationInput.value = '';
         ageInput.value = '';
 
-        // Hide the form and show the "Add New" button
         toggleFormVisibility();
     }
 });
 
-// Cancel button functionality
 cancelButton.addEventListener('click', function () {
-    // Hide the form and show the "Add New" button
     toggleFormVisibility();
 });
 
-// Clear button functionality
 clearButton.addEventListener('click', function () {
     nameInput.value = '';
     dateInput.value = '';
@@ -59,10 +52,8 @@ clearButton.addEventListener('click', function () {
     ageInput.value = '';
 });
 
-// Show form when "Add New" button is clicked
 addButton.addEventListener('click', toggleFormVisibility);
 
-// Validate form function
 function validateForm() {
     if (!nameInput.value || !dateInput.value || !designationInput.value || !ageInput.value) {
         alert('Please fill in all the fields.');
@@ -71,7 +62,6 @@ function validateForm() {
     return true;
 }
 
-// Delete button functionality for each row
 document.querySelector('table').addEventListener('click', function (event) {
     if (event.target.classList.contains('deleteButton')) {
         event.target.closest('tr').remove();
